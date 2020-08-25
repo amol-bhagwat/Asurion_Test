a34c174cf94e3ca50b55bd6051de5cf5d5808a35
AmolBAndroid <amolbhagwat23@gmail.com>
2020-08-25 10:35:38 +0530

    json files removed

--- app/src/main/java/com/example/asurion_test/util/Constant.kt
+++ app/src/main/java/com/example/asurion_test/util/Constant.kt
@@ -3,11 +3,10 @@ package com.example.asurion_test.util
 class Constant {
     companion object {
         const val CONFIG_URL =
-            "https://raw.githubusercontent.com/amol-bhagwat/Asurion_Test/master/app/src/main/res/assets/config.json"
+            "https://raw.githubusercontent.com/amol-bhagwat/TempApplication/master/app/src/main/res/assets/config.json"
         const val PET_URL =
-            "https://raw.githubusercontent.com/amol-bhagwat/Asurion_Test/master/app/src/main/res/assets/pets.json"
+            "https://raw.githubusercontent.com/amol-bhagwat/TempApplication/master/app/src/main/res/assets/pets.json"
         const val URL = "URL"
-
     }
 
 }
